046d629cb698bdf4ac9055837c894488439f1cdd
Justin <justin.espinas@gmail.com>
2019-04-19 18:01:03 +1200

    Sets up dotnet console

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace yugioh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blue = 6;
+            var yellow = 2;
+            var name = "Josh";
+
+            Console.WriteLine(blue + yellow);
+            System.Console.WriteLine(name);
+        }
+    }
+}
